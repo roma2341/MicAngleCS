1c9b8f785f7b933c647920b38a91ff3bb9746f91
zigzag2341@gmail.com <zigzag2341@gmail.com>
2017-07-06 13:56:23 +0300

    SimpleAngle created

---
+++ SimpleAngle/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SimpleAngle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnShowDiagrams = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tbIterations = new System.Windows.Forms.TextBox();
+            this.comboWaveInDeviceA = new System.Windows.Forms.ComboBox();
+            this.comboWaveInDeviceB = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShowDiagrams
+            // 
+            this.btnShowDiagrams.Location = new System.Drawing.Point(13, 1);
+            this.btnShowDiagrams.Name = "btnShowDiagrams";
+            this.btnShowDiagrams.Size = new System.Drawing.Size(116, 23);
+            this.btnShowDiagrams.TabIndex = 0;
+            this.btnShowDiagrams.Text = "Show diagrams";
+            this.btnShowDiagrams.UseVisualStyleBackColor = true;
+            this.btnShowDiagrams.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(13, 30);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(544, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // tbIterations
+            // 
+            this.tbIterations.Location = new System.Drawing.Point(135, 3);
+            this.tbIterations.Name = "tbIterations";
+            this.tbIterations.Size = new System.Drawing.Size(100, 20);
+            this.tbIterations.TabIndex = 2;
+            // 
+            // comboWaveInDeviceA
+            // 
+            this.comboWaveInDeviceA.FormattingEnabled = true;
+            this.comboWaveInDeviceA.Location = new System.Drawing.Point(588, 30);
+            this.comboWaveInDeviceA.Name = "comboWaveInDeviceA";
+            this.comboWaveInDeviceA.Size = new System.Drawing.Size(160, 21);
+            this.comboWaveInDeviceA.TabIndex = 3;
+            // 
+            // comboWaveInDeviceB
+            // 
+            this.comboWaveInDeviceB.FormattingEnabled = true;
+            this.comboWaveInDeviceB.Location = new System.Drawing.Point(588, 68);
+            this.comboWaveInDeviceB.Name = "comboWaveInDeviceB";
+            this.comboWaveInDeviceB.Size = new System.Drawing.Size(160, 21);
+            this.comboWaveInDeviceB.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 322);
+            this.Controls.Add(this.comboWaveInDeviceB);
+            this.Controls.Add(this.comboWaveInDeviceA);
+            this.Controls.Add(this.tbIterations);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnShowDiagrams);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowDiagrams;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox tbIterations;
+        private System.Windows.Forms.ComboBox comboWaveInDeviceA;
+        private System.Windows.Forms.ComboBox comboWaveInDeviceB;
+    }
+}
+
---
+++ SimpleAngle/Form1.cs
@@ -0,0 +1,177 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleAngle
+{
+    public partial class Form1 : Form
+    {
+        WaveInEvent waveInA, waveInB;
+
+        const int SAMPLING_RATE = 44100;
+        const int CHANNELS = 2;
+
+        Stopwatch stopwatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int waveInDevicesCount = WaveIn.DeviceCount;
+            for (int uDeviceID = 0; uDeviceID < waveInDevicesCount; uDeviceID++)
+            {
+                WaveInCapabilities waveInCaps = WaveIn.GetCapabilities(uDeviceID);
+                String productName = waveInCaps.ProductName;
+                comboWaveInDeviceA.Items.Add(productName);
+               // comboWaveInDeviceB.Items.Add(productName);
+            }
+
+
+            comboWaveInDeviceA.SelectedIndex = 0;
+           /* if (waveInDevicesCount > 1)
+                comboWaveInDeviceB.SelectedIndex = 1;
+            else
+                comboWaveInDeviceB.SelectedIndex = 0;*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BeginRecording();
+        }
+
+        bool isRecording = false;
+
+        public void toggleRecordButton()
+        {
+            isRecording = !isRecording;
+        }
+
+        void StopRecording()
+        {
+            const int BYTE_IN_SAMPLE = 2;
+            //MessageBox.Show("StopRecording");
+                waveInA.StopRecording();
+                //waveInB.StopRecording();
+        }
+
+        void waveIn_DataAvailableA(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailableA), sender, e);
+                return;
+            }
+            //if (waveInCapturedA) return;
+            //signalFromMicrophonesA = e.Buffer;
+           // waveInStartTimeA = (long)(stopwatch.Elapsed.TotalMilliseconds * 1000000);
+           // waveInCapturedA = true;
+            /* if (this.InvokeRequired)
+             {
+                 this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+                 return;
+             }
+             else*/
+            //Записываем данные из буфера в файл
+            // const int bytesInOnePortion = 4;
+            // int bytesPerChannel = bytesInOnePortion / channels;
+            /*   string ouputStr = "WaveIn data:\n";
+               for (int i = 0; i < e.Buffer.Length; i++)
+               {
+                   ouputStr += e.Buffer[i]+" ";
+               }
+               System.IO.File.WriteAllText("WaveInSignal.txt", ouputStr);*/
+            int[,] signalFromMics = convertByteArrayToChanneled(e.Buffer,2);
+            // signalFromMicrophones.Add(signalFromMics);
+            //angleForm.processAngle(signalFromMics);               
+            // Thread.Sleep(4000);
+            // bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public int[,] convertByteArrayToChanneled(byte[] buffer,int channels)
+        {
+            int[,] result = new int[channels, buffer.Length / 2];
+            int i = 0;
+            for (int sample = 0; sample < buffer.Length / 4; sample++)
+            {
+                result[0,i] = BitConverter.ToInt16(buffer, i);
+                i += 2;
+                result[1,i] = BitConverter.ToInt16(buffer, i);
+                i += 2;
+            }
+            return result;
+        }
+
+        private void waveIn_RecordingStoppedA(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler(waveIn_RecordingStoppedA), sender, e);
+            }
+            else
+            {
+                waveInA.Dispose();
+                waveInA = null;
+            }
+            //waveInCapturedA = false;
+        }
+
+        public void BeginRecording()
+        {
+            //  cntEvent.Wait();
+            if (!isRecording)
+            {
+                toggleRecordButton();
+                int deviceIdA = comboWaveInDeviceA.SelectedIndex;
+                int deviceIdB = comboWaveInDeviceB.SelectedIndex;
+                //bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat(44000, 1));
+                try
+                {
+                        //  MessageBox.Show("Start Recording");
+
+                        Thread.CurrentThread.IsBackground = true;
+                        waveInA = new WaveInEvent();
+                        waveInB = new WaveInEvent();
+                        //Дефолтное устройство для записи (если оно имеется)
+                        waveInA.DeviceNumber = deviceIdA;
+                        waveInB.DeviceNumber = deviceIdB;
+                        //Прикрепляем к событию DataAvailable обработчик, возникающий при наличии записываемых данных
+                        waveInA.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailableA);
+                       // waveInB.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailableB);
+                        //Прикрепляем обработчик завершения записи
+                        waveInA.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStoppedA);
+                        //waveInB.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStoppedB);
+                        //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
+                        waveInA.WaveFormat = new WaveFormat(SAMPLING_RATE, CHANNELS);
+                       // waveInB.WaveFormat = new WaveFormat(angleForm.getSignalManager().SamplingRate, angleForm.getSignalManager().Channels);
+                        waveInA.BufferMilliseconds = 100;
+                        waveInB.BufferMilliseconds = 100;
+                        //Инициализируем объект WaveFileWriter
+                        // writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                        //Начало записи
+                        stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        waveInA.StartRecording();
+                        //waveInB.StartRecording();
+
+                    
+                }
+                catch (Exception ex)
+                { MessageBox.Show(ex.Message); }
+            }
+            else
+            {
+                StopRecording();
+            }
+        }
+
+    }
+}
